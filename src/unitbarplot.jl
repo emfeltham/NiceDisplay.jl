41933e695c54c4b68618807ad61ca7e52ab4bd0f
emfeltham <eric.feltham@yale.edu>
2023-11-08 16:21:35 -0500

    better input

--- src/unitbarplot.jl
+++ src/unitbarplot.jl
@@ -10,7 +10,8 @@ By-unit stacked barplot for distribution of (binary-) categorical-valued `thing`
 function unitbarplot(
     dfin, thing;
     wave = nothing, prop = true, unit = :village_code, 
-    cscheme = :tol_light
+    clrs = colorschemes[:tol_light],
+    colsize = Relative(2/3)
 )
 
     df = deepcopy(dfin)
@@ -44,13 +45,18 @@ function unitbarplot(
     end
 
     # :Set1_9
-    clrs = colorschemes[cscheme];
 
     ur = replace(string(unit), "_" => " ")
 
     fg = Figure();
-    l1 = fg[1,1] = GridLayout();
-    ax1 = Axis(l1[1,1], xlabel = ur);
+
+    lo = fg[1, 1:2] = GridLayout();
+    l1 = lo[1, 1] = GridLayout();
+    ll = lo[1, 2] = GridLayout();
+    ax1 = Axis(
+        l1[1,1], xlabel = ur,
+        ygridvisible = false, xgridvisible = false
+    );
 
     barplot!(
         ax1, df[!, unit], df[!, vr],
@@ -66,8 +72,15 @@ function unitbarplot(
     elements = [PolyElement(polycolor = clrs[i+1]) for i in 1:length(labels)]
 
     ttl = replace(string(thing), "_" => " ")
-    Legend(fg[1,2], elements, labels, ttl)
+    Legend(
+        ll[1, 1], elements, labels, ttl,
+        tellheight = false, tellwidth = false
+    )
 
+    colgap!(lo, 0)
+    colsize!(lo, 1, colsize)
+    ylims!(ax1, 0, 1)
+    xlims!(ax1, extrema(df[!, unit])...)
     hidedecorations!(ax1, ticks = false, ticklabels = false, label = false)
     hidexdecorations!(ax1, label = false)
 
