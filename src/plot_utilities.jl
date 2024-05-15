cfc27eab4ef93907d9c25132e719f069c9e9f215
emfeltham <eric.feltham@yale.edu>
2024-05-15 10:20:00 -0400

    colors

--- src/plot_utilities.jl
+++ src/plot_utilities.jl
@@ -6,20 +6,64 @@ global berlin = colorschemes[:berlin];
 
 export wc, oi, berlin
 
+# https://yaleidentity.yale.edu/guidelines/websites#:~:text=The%20standard%20colors%20for%20the,the%20page%20design%20requires%20it.
 global yale = (
-    lblue = parse(Colorant, "#63aaff"),
-    blue = parse(Colorant, "#00356b"),
-    ora = parse(Colorant, "#bd5319"),
-    dgrey = parse(Colorant, "#4a4a4a"),
-    mgrey = parse(Colorant, "#978d85")
+    blues = [
+        parse(Colorant, "#00356b"),
+        parse(Colorant, "#286dc0"),
+        parse(Colorant, "#63aaff"),
+    ],
+    grays = [
+        parse(Colorant, "#222222"),
+        parse(Colorant, "#4a4a4a"),
+        parse(Colorant, "#978d85"),
+        parse(Colorant, "#dddddd"),
+        parse(Colorant, "#f9f9f9")
+    ],
+    accent = [
+        parse(Colorant, "#5f712d"),
+        parse(Colorant, "#bd5319"),
+    ],
 );
 
 export yale
 
+# https://www.umass.edu/university-relations/brand/brand-elements
+global umass = (
+    brand = [parse(Colorant, "#881c1c"), parse(Colorant, "#212721")],
+    secondary = [
+        parse(Colorant, "#ffc72c"), parse(Colorant, "#ff9e1b"),
+        parse(Colorant, "#615e9b"), parse(Colorant, "#44693d"),
+        parse(Colorant, "#5e4b3c"), parse(Colorant, "#002554")
+    ],
+    neutrals = [
+        parse(Colorant, "#373a36"), parse(Colorant, "#505759"),
+        parse(Colorant, "#a2aaad")
+    ],
+);
+
+export umass
+
+# https://visualidentity.columbia.edu/branding#:~:text=To%20remain%20compliant%20with%20WCAG,components%20and%20large%20text%20only.
+global columbia = (
+    blue = [parse(Colorant, "#b9d9eb"), parse(Colorant, "#1d4f91")],
+    # secondary = [
+    #     parse(Colorant, "#ffc72c"), parse(Colorant, "#ff9e1b"),
+    #     parse(Colorant, "#615e9b"), parse(Colorant, "#44693d"),
+    #     parse(Colorant, "#5e4b3c"), parse(Colorant, "#002554")
+    # ],
+    # neutrals = [
+    #     parse(Colorant, "#373a36"), parse(Colorant, "#505759"),
+    #     parse(Colorant, "#a2aaad")
+    # ]
+);
+
+export columbia
+
 # functions to set common plotting parameters
 
-function yxline!(ax)
-    lines!(ax, 0:.01:1, 0:.01:1, color = :black, linestyle = :dot)
+function yxline!(ax; linestyle = :dot, color = :black, kwargs...)
+    lines!(ax, 0:.001:1, 0:.001:1; linestyle, color, kwargs...)
 end
 
 export yxline!
