5b0e616705bc05e0e9ebc09ecd2b108b6df77c76
emfeltham <eric.feltham@yale.edu>
2024-05-24 13:39:18 -0400

    colors

--- src/colors.jl
+++ src/colors.jl
@@ -5,6 +5,22 @@ global berlin = colorschemes[:berlin];
 
 export wc, oi, berlin
 
+# https://www.umass.edu/university-relations/brand/brand-elements
+global umass = (
+    brand = [parse(Colorant, "#881c1c"), parse(Colorant, "#212721")],
+    secondary = [
+        parse(Colorant, "#ffc72c"), parse(Colorant, "#ff9e1b"),
+        parse(Colorant, "#615e9b"), parse(Colorant, "#44693d"),
+        parse(Colorant, "#5e4b3c"), parse(Colorant, "#002554")
+    ],
+    neutrals = [
+        parse(Colorant, "#373a36"), parse(Colorant, "#505759"),
+        parse(Colorant, "#a2aaad")
+    ],
+);
+
+export umass
+
 # https://yaleidentity.yale.edu/guidelines/websites#:~:text=The%20standard%20colors%20for%20the,the%20page%20design%20requires%20it.
 global yale = (
     blues = [
@@ -27,9 +43,9 @@ global yale = (
 
 export yale
 
-# https://www.umass.edu/university-relations/brand/brand-elements
-global umass = (
-    brand = [parse(Colorant, "#881c1c"), parse(Colorant, "#212721")],
+# https://visualidentity.columbia.edu/branding#:~:text=To%20remain%20compliant%20with%20WCAG,components%20and%20large%20text%20only.
+global columbia = (
+    blue = [parse(Colorant, "#b9d9eb"), parse(Colorant, "#1d4f91")],
     secondary = [
         parse(Colorant, "#ffc72c"), parse(Colorant, "#ff9e1b"),
         parse(Colorant, "#615e9b"), parse(Colorant, "#44693d"),
@@ -38,23 +54,7 @@ global umass = (
     neutrals = [
         parse(Colorant, "#373a36"), parse(Colorant, "#505759"),
         parse(Colorant, "#a2aaad")
-    ],
-);
-
-export umass
-
-# https://visualidentity.columbia.edu/branding#:~:text=To%20remain%20compliant%20with%20WCAG,components%20and%20large%20text%20only.
-global columbia = (
-    blue = [parse(Colorant, "#b9d9eb"), parse(Colorant, "#1d4f91")],
-    # secondary = [
-    #     parse(Colorant, "#ffc72c"), parse(Colorant, "#ff9e1b"),
-    #     parse(Colorant, "#615e9b"), parse(Colorant, "#44693d"),
-    #     parse(Colorant, "#5e4b3c"), parse(Colorant, "#002554")
-    # ],
-    # neutrals = [
-    #     parse(Colorant, "#373a36"), parse(Colorant, "#505759"),
-    #     parse(Colorant, "#a2aaad")
-    # ]
+    ]
 );
 
 export columbia
