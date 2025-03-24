901cb74ff55ea2903b23ce5b3a20a8ca7ce04f78
emfeltham <eric.feltham@yale.edu>
2025-03-23 20:10:29 -0400

    update

--- src/NiceDisplay.jl
+++ src/NiceDisplay.jl
@@ -5,10 +5,16 @@ module NiceDisplay
     using DataFrames
     using LinearAlgebra
     using PrettyTables # for nicedisplay()
+
+    using HondurasTools
     
     @reexport using Colors
     @reexport using ColorSchemes
     @reexport using CairoMakie
+
+    using HondurasTools.Graphs
+    using HondurasTools.MetaGraphs
+    using GraphMakie
     
     for x in [
         "plot utilities.jl",
